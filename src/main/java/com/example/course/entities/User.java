e2840b18dc3e15794dcf0b1da2a965dfe177e9f2
Luciano Bessegato <lfrbessegato@gmail.com>
2019-09-26 23:38:45 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/example/course/entities/User.java
+++ src/main/java/com/example/course/entities/User.java
@@ -1,14 +1,20 @@
 package com.example.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 //Fazer a classe e depender da especificação
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-import javax.persistence.Id; 
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table; 
 
 @Entity
+@Table(name="tb_user")
 public class User implements Serializable{
 	
 	/**
@@ -23,6 +29,12 @@ public class User implements Serializable{
 	private String email;
 	private String phone;
 	private String password;
+	
+	/* 
+	 * Para fazer a associação entre user e order
+	 * */
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	/* 
 	 * Como está usando com Framework obrigatório criar o construtor vazio
@@ -78,6 +90,10 @@ public class User implements Serializable{
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
 
 	@Override
 	public int hashCode() {
@@ -103,9 +119,4 @@ public class User implements Serializable{
 			return false;
 		return true;
 	}
-	
-	
-	
-	
-
 }
